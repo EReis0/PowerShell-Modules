d10aadc1505266966d89a8dbabcb78b47417ac19
TheBleak13 <34757963+thebleak13@users.noreply.github.com>
2022-10-12 10:58:01 -0700

    Improved and converted into a function

--- Custom Module Resources/Install-custom-module.ps1
+++ Custom Module Resources/Install-custom-module.ps1
@@ -7,19 +7,47 @@ You don't really need this script, you can technically just copy the folder and 
 This will also avoid the issues with AV.
 
 #>
-$WindowsPowerShellModulePath = $env:PSModulePath.Split(';')[0]
-$ModulePath = "$WindowsPowerShellModulePath\BleakKitchenSink"
+Function Install-CustomModule {
+    <#
+    .SYNOPSIS
+        Installs a custom module from the downloads folder to the module directory.
+    .DESCRIPTION
+        Installs a custom module from the downloads folder to the module directory.
+    .EXAMPLE
+        Install-CustomModule -ModuleName <string>
+    .NOTES
+        N/A
+    .LINK
+        https://github.com/thebleak13/PowerShell-Samples/blob/main/Modules/BleakKitchenSink/README.md
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ModuleName
+    )
 
-$DownloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-$InstallCustomModulePath = "$DownloadsPath\BleakKitchenSink\"
+    $WindowsPowerShellModulePath = $env:PSModulePath.Split(';')[0]
+    $ModulePath = "$WindowsPowerShellModulePath\$ModuleName"
 
-$filecheck = test-path $ModulePath
-if ($filecheck) {
-    Remove-Item -Path $ModulePath -Force -Recurse
-}
+    $DownloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+    $InstallCustomModulePath = "$DownloadsPath\$ModuleName\"
 
-try{
-Copy-Item -Path $InstallCustomModulePath -Destination $ModulePath -PassThru -Recurse
-}catch{
-    $_.Exception.Message
+    Write-Host "This script will install the $ModuleName module from the downloads folder to the module directory." -ForegroundColor Black -BackgroundColor white
+    Write-Host "Copy module from $ModulePath to $WindowsPowerShellModulePath" -ForegroundColor Black -BackgroundColor white
+    Write-Host "If you have an Anti-Virus, it may flag this script as a virus. You will have to exclude powershell.exe from your AV during the installation." -ForegroundColor Black -BackgroundColor Yellow
+
+    $filecheck = test-path $ModulePath
+    if ($filecheck) {
+        Remove-Item -Path $ModulePath -Force -Recurse
+    }
+
+    try{
+    Copy-Item -Path $InstallCustomModulePath -Destination $ModulePath -PassThru -Recurse -ErrorAction Stop
+    Write-Host $ModuleName Was successfully installed to $WindowsPowerShellModulePath -ForegroundColor Black -BackgroundColor Green
+    }catch{
+        $CatchError = $_.Exception.Message
+        $ErrorType = $CatchError[0].Exception.GetType().FullName
+        Write-Host $CatchError -ForegroundColor Black -BackgroundColor Red
+        Write-Host $ErrorType -ForegroundColor Black -BackgroundColor Red
+    }
 }
