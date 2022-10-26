5feda290d81d876216a613bb99da354146e0613f
TheBleak13 <34757963+thebleak13@users.noreply.github.com>
2022-10-25 18:03:33 -0700

    Update BleakKitchenSink.psm1

--- Modules/BleakKitchenSink/BleakKitchenSink.psm1
+++ Modules/BleakKitchenSink/BleakKitchenSink.psm1
@@ -1,8 +1,30 @@
-#requires -Module 'PSWriteHTML'
+Function Convert-CSVtoHTML {
+    <#
+    .SYNOPSIS
+        Convert-CSVtoHTML
+    .DESCRIPTION
+        Convert a CSV file to an HTML report using PSWriteHTML and preferred params.
 
-#Importing required modules
-Import-module -Name 'PSWriteHTML'
+        - This function is a wrapper for the ConvertTo-HTMLTable function in PSWriteHTML. 
+        When the function is executed, a file browser will open to select the CSV file to convert. 
 
+        In order to use this, you must have PSWriteHTML installed.
+    .NOTES
+        - Could include the code from Get-CSVFilePath to reduce the lines of code for this feature.
+    .LINK
+        https://github.com/thebleak13/PowerShell-Samples/blob/main/BleakKitchenSink/README.md
+    #>
+    Import-Module -Name PSWriteHTML
+    $DownloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+    $File = Get-CSVFilePath
+    $importFile = @(Import-CSV -path $File)
+    $FileNameRaw = (Get-ChildItem $file).Name
+    $FileName = $FileNameRaw.Replace(".csv",'')
+    Write-Host "`n`r ---> Exported report to $DownloadsPath\$FileName.html" -foregroundcolor 'black' -backgroundcolor 'green'
+    New-HTML {
+        New-HTMLTable -DataTable $importFile -Title $FileName -HideFooter -PagingLength 10 -Buttons excelHtml5,searchPanes
+    } -ShowHTML -FilePath "$DownloadsPath\$FileName.html" -Online
+} #Convert-CSVtoHTML
 Function Get-AskUserYNQuestion {
     <#
     .SYNOPSIS
@@ -44,33 +66,7 @@ Function Get-AskUserYNQuestion {
             }
          }
          return $Answer
-} #Get-AskUserYNQuestion -Question 'Are you ready?'
-
-Function Get-Folder($initialDirectory){
-<#
-.SYNOPSIS
-    Get-Folder
-.DESCRIPTION
-    Opens a folder browser dialog and returns the selected folder.
-.NOTES
-    N/A
-.LINK
-    https://github.com/thebleak13/PowerShell-Samples/blob/main/BleakKitchenSink/README.md
-#>
-    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-
-    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
-    $foldername.Description = "Select a folder"
-    $foldername.rootfolder = "MyComputer"
-    $foldername.SelectedPath = $initialDirectory
-
-    if($foldername.ShowDialog() -eq "OK"){
-    $folder += $foldername.SelectedPath
-    }
-    
-    return $folder
-} 
-
+}
 Function Get-CSVFilePath($initialDirectory){
     <#
     .SYNOPSIS
@@ -89,29 +85,27 @@ Function Get-CSVFilePath($initialDirectory){
     $OpenFileDialog.ShowDialog() | Out-Null
     $OpenFileDialog.filename
 }
-
-Function Convert-CSVtoHTML {
+Function Get-Folder($initialDirectory){
     <#
     .SYNOPSIS
-        Convert-CSVtoHTML
+        Get-Folder
     .DESCRIPTION
-        Convert a CSV file to an HTML report using PSWriteHTML and preferred params.
-
-        - This function is a wrapper for the ConvertTo-HTMLTable function in PSWriteHTML. 
-        When the function is executed, a file browser will open to select the CSV file to convert. 
+        Opens a folder browser dialog and returns the selected folder.
     .NOTES
-        - Could include the code from Get-CSVFilePath to reduce the lines of code for this feature.
+        N/A
     .LINK
         https://github.com/thebleak13/PowerShell-Samples/blob/main/BleakKitchenSink/README.md
     #>
-    [CmdletBinding()]
-    $DownloadsPath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-    $File = Get-CSVFilePath
-    $importFile = @(Import-CSV -path $File)
-    $FileNameRaw = (Get-ChildItem $file).Name
-    $FileName = $FileNameRaw.Replace(".csv",'')
-    Write-Host "`n`r ---> Exported report to $DownloadsPath\$FileName.html" -foregroundcolor 'black' -backgroundcolor 'green'
-    New-HTML {
-        New-HTMLTable -DataTable $importFile -Title $FileName -HideFooter -PagingLength 10 -Buttons excelHtml5,searchPanes
-    } -ShowHTML -FilePath "$DownloadsPath\$FileName.html" -Online
-} #Convert-CSVtoHTML
+        [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+        $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+        $foldername.Description = "Select a folder"
+        $foldername.rootfolder = "MyComputer"
+        $foldername.SelectedPath = $initialDirectory
+    
+        if($foldername.ShowDialog() -eq "OK"){
+        $folder += $foldername.SelectedPath
+        }
+        
+        return $folder
+    }
