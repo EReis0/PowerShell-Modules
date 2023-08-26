446e791c21893378f2044052a08b0e67a30c50f9
TheBleak <34757963+thebleak13@users.noreply.github.com>
2023-08-26 10:44:15 -0700

    Update test.ps1

--- Custom Module Resources/test.ps1
+++ Custom Module Resources/test.ps1
@@ -1,5 +1,34 @@
-Import-Module -Name BleakKitchenSink
+# Every Script include this
+#$modulePath = "C:\Program Files\WindowsPowerShell\Modules\KitchenSink"
+#$env:PSModulePath = "C:\Users\thebl\Documents\WindowsPowerShell\Modules;C:\Program Files\WindowsPowerShell\Modules;C:\WINDOWS\system32\WindowsPowerShell\v1.0\Modules;c:\Users\thebl\.vscode\extensions\ms-vscode.powershell-2023.6.0\modules;"
 
-Get-Module -Name BleakKitchenSink
 
-Get-Command -Module BleakKitchenSink
+
+# Run this to create the profile
+$ProfilePath = "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+$CheckProfile = Test-Path -Path $ProfilePath
+if ($CheckProfile -eq $false) {
+    New-Item -ItemType File -Path $ProfilePath -Force
+}
+
+# Check if the Import-Module command already exists in the profile
+$ProfileContent = Get-Content -Path $ProfilePath
+$ImportModuleCommand = "Import-Module -Name KitchenSink"
+$CommandExists = $ProfileContent -like "*$ImportModuleCommand*"
+
+# Open the profile in notepad
+#notepad $Profile
+
+# If the command does not exist, add it to the profile
+if (-not $CommandExists) {
+    Add-Content -Path $ProfilePath -Value "`n$ImportModuleCommand`n"
+}
+
+# Add this to the profile
+# Import-Module -Name KitchenSink
+
+Import-Module -Name KitchenSink
+
+Get-Module -Name KitchenSink
+
+Get-Command -Module KitchenSink
