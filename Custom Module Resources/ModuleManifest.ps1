928716fc296d041ad1be80e2c45b9ada19cbb268
TheBleak <34757963+thebleak13@users.noreply.github.com>
2023-08-28 20:26:33 -0700

    Update ModuleManifest.ps1

--- Custom Module Resources/ModuleManifest.ps1
+++ Custom Module Resources/ModuleManifest.ps1
@@ -2,27 +2,28 @@ $RootDir = "D:\Code\Repos\PowerShell-Modules\Modules\KitchenSink"
 $FunctionDir = "$RootDir\Functions"
 
 # Get all functions in the Functions folder
-$Functions = @()
-Get-ChildItem -Path $FunctionDir -Filter *.ps1 -Recurse | ForEach-Object {
+$Functions = Get-ChildItem -Path $FunctionDir -Filter *.ps1 -Recurse | ForEach-Object {
     $content = Get-Content $_.FullName
     foreach ($item in $content) {
-        if ($item -match "^function\s+([a-z]+\-[a-zA-Z]+)") {
-            $Functions += $matches[1]
+        if ($item -match "^function\s+([a-z]+[\w-]*)") {
+            $matches[1]
         }
     }
-}
+} | Select-Object -Unique
+
+$ModuleName = $RootDir.Split("\")[-1]
 
 # Create the module manifest
 $Params = @{ 
-    "Path" 				        = "$RootDir\KitchenSink.psd1"
+    "Path" 				        = "$RootDir\$ModuleName.psd1"
     "Author" 			        = 'https://github.com/thebleak13' 
     "CompanyName" 			    = 'Codeholics.com' 
-    "RootModule" 			    = 'KitchenSink.psm1' 
+    "RootModule" 			    = "$ModuleName.psm1"
     "CompatiblePSEditions" 		= @('Desktop','Core') 
     "FunctionsToExport" 		= $Functions
     "CmdletsToExport" 		    = $Functions
     "VariablesToExport" 		= '' 
     "AliasesToExport" 		    = @() 
-    "Description"               = 'KitchenSink' 
+    "Description"               = "$ModuleName is a custom built module."
 } 
 New-ModuleManifest @Params
