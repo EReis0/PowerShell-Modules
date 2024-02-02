cc1335e9e7df39e39f901dd155806d78d640fc51
TheBleak <thebleak13@gmail.com>
2024-02-02 16:05:46 -0700

    Update New-CredsTxtFile.ps1

--- Modules/KitchenSink/Functions/New-CredsTxtFile.ps1
+++ Modules/KitchenSink/Functions/New-CredsTxtFile.ps1
@@ -1,30 +1,32 @@
 <#
 .SYNOPSIS
-    Exports a password to a file in UTF8 encoding and validates the file.
-.DESCRIPTION
-    Exports a password to a file in UTF8 encoding and validates the file. The function converts the password to a secure string and then to an encrypted standard string using the ConvertTo-SecureString and ConvertFrom-SecureString cmdlets. The encrypted string is then written to a file using the Out-File cmdlet. The function also validates the file by comparing the decrypted password to the original password.
-.PARAMETER Filepath
-    Specifies the path and filename of the file to export the password to.
-.PARAMETER Password
-    Specifies the password to export.
-.PARAMETER ValidateOnly
-    Only validates if the encypted password in the file matches the specified password. 
-    Using this parameter will not export the password to the file.
-    If you use this parameter you will need to specify the value as $true to validate only.
-.EXAMPLE
-    PS C:\> New-CredsTxtFile -Filepath "C:\Temp\password.txt" -Password "MyPassword123"
-    Exports the password "MyPassword123" to the file "C:\Temp\password.txt" in UTF8 encoding and validates the file.
-.EXAMPLE
-    PS C:\> New-CredsTxtFile -Filepath "C:\Temp\password.txt" -Password "MyPassword123" -ValidateOnly
-    Validates the file "C:\Temp\password.txt" by comparing the decrypted password to the original password.
-.NOTES
-    This function exports a password to a file in UTF8 encoding and validates the file. The function converts the password to a secure string and then to an encrypted standard string using the ConvertTo-SecureString and ConvertFrom-SecureString cmdlets. The encrypted string is then written to a file using the Out-File cmdlet. The function also validates the file by comparing the decrypted password to the original password.
-    
-    Author: Codeholics (https://github.com/Codeholics) - Eric Reis (https://github.com/EReis0/)
-    Version: 1.0
-    Date: 01/16/2024
+    A function to create a password file or validate a password against a file.
 
-    This function is not really needed but a good way to create and validate a password file in a consistent manner.
+.DESCRIPTION
+    The New-CredsTxtFile function prompts the user for a password and saves it to a file, or validates a password against a file. 
+    If the -ValidateOnly parameter is specified, the function validates the password against the file. 
+    If the -ValidateOnly parameter is not specified, the function prompts the user for a password and saves it to the file.
+
+.PARAMETER Filepath
+    The path of the file to save the password to or validate the password against.
+
+.PARAMETER ValidateOnly
+    If specified, the function validates the password against the file. 
+    If not specified, the function prompts the user for a password and saves it to the file.
+
+.EXAMPLE
+    New-CredsTxtFile -Filepath "C:\creds\sample55.txt" -ValidateOnly $true
+
+    This command validates the password entered by the user against the password in the file at C:\creds\sample55.txt.
+
+.EXAMPLE
+    New-CredsTxtFile -Filepath "C:\creds\sample55.txt"
+
+    This command prompts the user for a password and saves it to the file at C:\creds\sample55.txt.
+.NOTES
+    Author: Codeholics (https://github.com/Codeholics) - Eric Reis (https://github.com/EReis0/)
+    Version: 1.2
+    Date: 02/02/2024
 .LINK
     https://github.com/EReis0/PowerShell-Samples/
 #>
@@ -33,32 +35,58 @@ function New-CredsTxtFile {
     param (
         [Parameter(Mandatory=$true)]
         [string]$Filepath,
-        [Parameter(Mandatory=$true)]
-        [string]$Password,
         [Parameter(Mandatory=$false)]
         [bool]$ValidateOnly = $false
     )
-    
+
+    # If -validateonly is not specified, prompt for password and save it to file
     if ($ValidateOnly -eq $false) {
-    # Save the password
-    Write-Host "Exporting Password to $Filepath" -foregroundcolor green
-    ConvertTo-SecureString -String $Password -AsPlainText -Force | ConvertFrom-SecureString | Out-File $Filepath -Encoding UTF8
+
+        # Prompt for password
+        $Credential = Get-Credential -Message "Enter your password" -UserName "Admin"
+
+        # If the credential prompt is cancelled, exit the function
+        if ($null -eq $Credential) {
+            Write-Warning "Credential prompt was cancelled. Exiting..."
+            return
+        } else {
+            $Password = $Credential.GetNetworkCredential().Password
+        }
+
+        # Save the password
+        Write-Host "Exporting Password to $Filepath" -foregroundcolor green
+        ConvertTo-SecureString -String $Password -AsPlainText -Force | ConvertFrom-SecureString | Out-File $Filepath -Encoding UTF8
+    # If -validateonly is specified, validate the file
+    } elseif ($ValidateOnly -eq $true) {
+
+        # Verify file path exists
+        $PathValidation = Test-Path -Path $Filepath
+        if ($PathValidation -eq $false) {
+            Write-Warning "File does not exist. Please specify a valid file path."
+            return
+        }
+
+        # Prompt for password
+        $Credential = Get-Credential -Message "Enter your password" -UserName "Admin"
+        
+        # If the credential prompt is cancelled, exit the function
+        if ($null -eq $Credential) {
+            Write-Warning "Credential prompt was cancelled. Exiting..."
+            return
+        } else {
+            $Password = $Credential.GetNetworkCredential().Password
+        }
+
+        # Verify password file
+        $SecureString = ConvertTo-SecureString -String (Get-Content $Filepath)
+        $Pointer = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
+        $SecretContent = [Runtime.InteropServices.Marshal]::PtrToStringAuto($Pointer)
+
+        # Compare the password for a match
+        if ($SecretContent -eq $Password) {
+            Write-Host "Password MATCHED decrypted password." -foregroundcolor green
+        } else {
+            Write-Warning "Password DID NOT MATCH decrypted password."
+        }
     }
-
-    # Verify password file
-    $SecureString = ConvertTo-SecureString -String (Get-Content $Filepath)
-    $Pointer = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
-    $SecretContent = [Runtime.InteropServices.Marshal]::PtrToStringAuto($Pointer)
-
-    # Display the password in plain text
-    # $SecretContent
-
-    # Compare the password for a match
-    if ($SecretContent -eq $Password) {
-        Write-Host "Password MATCHED decrypted password." -foregroundcolor green
-    } else {
-        Write-Warning "Password DID NOT MATCH decrypted password."
-    }
-}  # New-CredsTxtFile -Filepath "C:\creds\sample.txt" -Password "P@ssw0rd" -ValidateOnly $true
-
-
+}  # New-CredsTxtFile -Filepath "C:\creds\sample.txt" -ValidateOnly $true
