2a2ed83eddbbba387ac3739896bcfc26340432d8
Eric Reis <34757963+EReis0@users.noreply.github.com>
2025-04-14 11:38:30 -0700

    Update Install-CustomModule.ps1
    - Updated Get-Help comments
    - Added feature that will unblock the module if it's not currently signed by using `-unblock`
    - suppressed directory listings and non-useful text from console output
    - Updated the path logic for the powershell profile to not use hardcoded paths so it is more flexible with non-standard profiles.
    - Improved importing the module to use regex instead of using the full file context to boot performance
    - Simplfied the submodel installation instead of using a more complex method.

--- Modules/KitchenSink/Functions/Install-CustomModule.ps1
+++ Modules/KitchenSink/Functions/Install-CustomModule.ps1
@@ -1,31 +1,53 @@
 <#
 .SYNOPSIS
-    Installs a custom module from the downloads folder to the module directory.
+    Installs a custom PowerShell module to the appropriate module directory.
+
 .DESCRIPTION
-    This function installs a custom module from the downloads folder to the module directory. 
-    The function copies the module folder from the downloads folder to the module directory, 
-    and removes any existing module with the same name. If an Anti-Virus is running, it may flag this script as a virus. 
-    You will have to exclude powershell.exe from your AV during the installation.
+    This function installs a custom PowerShell module by copying the module folder from the specified input directory 
+    to the appropriate module directory based on the user level. It also ensures the module is imported into the user's 
+    PowerShell profile for automatic loading in future sessions. Optionally, it can unblock unsigned scripts.
+
 .PARAMETER InputDir
-    Specifies the path of the module to install. This parameter is mandatory. It should be the path to your module folder.
+    Specifies the path of the module to install. This parameter is mandatory and should point to the folder containing 
+    the module files.
+
 .PARAMETER UserLevel
     Specifies the level of the user for which the module should be installed. 
-    This parameter is optional and can be set to either 'Single' or 'All'. If not specified, the default value is 'All'.
-.EXAMPLE
-    Install-CustomModule -InputDir "C:\Users\thebl\Downloads\KitchenSink" -UserLevel "All"
-    This example installs the "KitchenSink" module from the -InputDir  to the module directory for all users.
+    Valid values are:
+        - 'Single': Installs the module for the current user only.
+        - 'All': Installs the module for all users on the system.
+    The default value is 'All'.
 
-    Install-CustomModule -InputDir "C:\Users\thebl\Downloads\KitchenSink" -UserLevel "Single"
-    This example installs the "KitchenSink" module from the -InputDir  to the module directory for the current user.
+.PARAMETER Unblock
+    Indicates whether to unblock the module script if it is not signed. This parameter is optional. 
+    If specified, the script will check the signature status of the module and unblock it if necessary.
+
+.EXAMPLE
+    Install-CustomModule -InputDir "C:\Users\JohnDoe\Downloads\MyModule" -UserLevel "All"
+    Installs the "MyModule" module from the specified input directory to the module directory for all users.
+
+.EXAMPLE
+    Install-CustomModule -InputDir "C:\Users\JohnDoe\Downloads\MyModule" -UserLevel "Single"
+    Installs the "MyModule" module from the specified input directory to the module directory for the current user only.
+
+.EXAMPLE
+    Install-CustomModule -InputDir "C:\Users\JohnDoe\Downloads\MyModule" -UserLevel "All" -Unblock
+    Installs the "MyModule" module for all users and unblocks the script if it is not signed.
+
 .NOTES
     Author: Codeholics (https://github.com/Codeholics) - Eric Reis (https://github.com/EReis0/)
-    Version: 1.2
-    Date: 10/2023
+    Version: 1.4
+    Date: 04/2025
 
-    You don't really need this script, you can technically just copy the folder and paste it to the module directory 
-    'C:\Program Files\WindowsPowerShell\Modules'. Then you will need to add the import-module command to your profile.
-    
-    Installing the module is useful but you can also just import-module with the path to use it for a script.
+    This script simplifies the process of installing PowerShell modules by automating the following steps:
+        - Copying the module to the appropriate module directory.
+        - Adding the module to the user's PowerShell profile for automatic loading.
+        - Unblocking unsigned scripts if requested.
+
+    While this script is useful for automating module installation, you can also manually copy the module folder 
+    to the module directory (e.g., 'C:\Program Files\WindowsPowerShell\Modules') and add the `Import-Module` command 
+    to your profile.
+
 .LINK
     https://github.com/EReis0/PowerShell-Samples/
 #>
@@ -35,7 +57,9 @@ Function Install-CustomModule {
         [Parameter(Mandatory=$true)]
         $InputDir,
         [Parameter(Mandatory=$false)]
-        $UserLevel = 'All'
+        $UserLevel = 'All',
+        [Parameter(Mandatory=$false, HelpMessage="Unblock the script if it is not signed.")]
+        [switch]$Unblock
     )
 
     # Get the module name from the input directory
@@ -52,7 +76,6 @@ Function Install-CustomModule {
 
     $ModuleOutputPath = Join-Path -Path $WindowsPowerShellModulePath -ChildPath $ModuleName
     
-    Write-Host "This script will install the $ModuleName module to the module directory." -ForegroundColor Black -BackgroundColor white
     Write-Host "Copy module from $InputDir to $WindowsPowerShellModulePath" -ForegroundColor Black -BackgroundColor white
 
     # Check if the module is already installed
@@ -65,10 +88,38 @@ Function Install-CustomModule {
     # Add the directory containing the module to the PSModulePath environment variable
     $env:PSModulePath += ";$InputDir"
 
+    # Unblock the script if the -Unblock parameter is used
+    if ($Unblock) {
+        Write-Host "Checking script signature and unblocking if necessary..." -ForegroundColor Yellow
+
+        # Replace with the path to your script file
+        $scriptPath = Join-Path -Path $InputDir -ChildPath "$($ModuleName).psm1"
+
+        # Check the signature status
+        $signature = Get-AuthenticodeSignature -FilePath $scriptPath
+
+        # Display the signature status and unblock if needed
+        if ($signature.Status -eq 'Valid') {
+            Write-Host "The script is signed and valid." -ForegroundColor Green
+        } elseif ($signature.Status -eq 'NotSigned') {
+            Write-Host "The script is not signed. Unblocking the file..." -ForegroundColor Yellow
+            Unblock-File -Path $scriptPath
+            Write-Host "The file has been unblocked." -ForegroundColor Green
+        } else {
+            Write-Host "The script is signed but the signature is invalid or unknown." -ForegroundColor Red
+        }
+    }
+
+    # Validate InputDir
+    if (-not (Test-Path -Path $InputDir -PathType Container)) {
+        Write-Host "The specified InputDir '$InputDir' does not exist or is not a directory." -ForegroundColor Red
+        return
+    }
+
     # Copy the module to the module directory
     try{
-    Copy-Item -Path $InputDir -Destination $ModuleOutputPath -PassThru -Recurse -ErrorAction Stop
-    Write-Host "$ModuleName Was successfully installed to $WindowsPowerShellModulePath" -ForegroundColor Black -BackgroundColor Green
+        Copy-Item -Path $InputDir -Destination $ModuleOutputPath -PassThru -Recurse -ErrorAction Stop | Out-Null
+        Write-Host "$ModuleName Was successfully installed to $WindowsPowerShellModulePath" -ForegroundColor Black -BackgroundColor Green
     }catch{
         $CatchError = $_.Exception.Message
         $ErrorType = $CatchError[0].Exception.GetType().FullName
@@ -77,48 +128,43 @@ Function Install-CustomModule {
     }
 
     # Run this to create the profile
-    $ProfilePath = "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
-    $CheckProfile = Test-Path -Path $ProfilePath
-    if ($CheckProfile -eq $false) {
-        Write-Host "Creating profile at $ProfilePath" -ForegroundColor Black -BackgroundColor Yellow
-        New-Item -ItemType File -Path $ProfilePath -Force
+    $ProfilePath = $profile.CurrentUserCurrentHost # $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+    try {
+        $CheckProfile = Test-Path -Path $ProfilePath
+        if ($CheckProfile -eq $false) {
+            Write-Host "Creating profile at $ProfilePath" -ForegroundColor Black -BackgroundColor Yellow
+            New-Item -ItemType File -Path $ProfilePath -Force
+        }
+    } catch {
+        Write-Host "Error creating profile: $($_.Exception.Message)" -ForegroundColor Red
+        return
     }
 
     # Check if the Import-Module command already exists in the profile
     Write-Host "Checking if $ModuleName is already imported in the profile" -ForegroundColor Black -BackgroundColor Yellow
 
-    $ProfileContent = Get-Content -Path $ProfilePath
-
     # If the command (for main module) does not exist, add it to the profile
     $ImportModuleCommand = "Import-Module -Name $ModuleName"
-    $CommandExists = $ProfileContent -like "*$ImportModuleCommand*"
-    if (-not $CommandExists) {
+    $EscapedPattern = [regex]::Escape($ImportModuleCommand) # Escape special characters in the command
+    if (-not (Select-String -Path $ProfilePath -Pattern $EscapedPattern -Quiet)) {
         Write-Host "Adding $ModuleName to the profile" -ForegroundColor Black -BackgroundColor Yellow
         Add-Content -Path $ProfilePath -Value "$ImportModuleCommand"
     }
 
     # Find Submodules
     $SubModules = @(Get-ChildItem -Path $ModuleOutputPath -Recurse -Filter '*.psm1' |
-    Where-Object {$_.BaseName -ne $ModuleName} | Select-Object Name,BaseName,FullName)
+    Where-Object {$_.BaseName -ne $ModuleName} | Select-Object Name,BaseName,FullName | Out-Null)
 
+    # Add submodules to the profile
     if ($SubModules) {
-        # Add each submodule to the profile
         foreach ($SubModule in $SubModules) {
-            $Name = $SubModule.Name
-            $FullName = $SubModule.FullName
-            $BaseName = $SubModule.BaseName
-            $ImportSubModuleCommand = "Import-Module ""$FullName"""
-            $ImportSubModuleCommand2 = Join-path -path $WindowsPowerShellModulePath -ChildPath $modulename | Join-Path -ChildPath "$($modulename).psm1"  
-            $CommandExists = $ProfileContent -like "*$ImportSubModuleCommand*" -or $ProfileContent -like "*$ImportSubModuleCommand2*"
-
-            # If the command (for submodules) does not exist, add it to the profile
-            if (-not $CommandExists) {
-                Write-Host "Adding $SubModule to the profile" -ForegroundColor Black -BackgroundColor Yellow
-                Add-Content -Path $ProfilePath -Value "$ImportSubModuleCommand"
+            $ImportSubModuleCommand = "Import-Module -Name '$($SubModule.FullName)'"
+            if (-not (Select-String -Path $ProfilePath -Pattern [regex]::Escape($ImportSubModuleCommand) -Quiet)) {
+                Write-Host "Adding submodule $($SubModule.BaseName) to the profile" -ForegroundColor Black -BackgroundColor Yellow
+                Add-Content -Path $ProfilePath -Value $ImportSubModuleCommand
             }
         }
-}
-
+    }
 
     Write-host "Validating Installation..." -ForegroundColor Yellow
 
@@ -161,4 +207,4 @@ Function Install-CustomModule {
         }
     }
 }  # Install-CustomModule -InputDir 'D:\Code\Repos\PowerShell-Modules\Modules\KitchenSink' -UserLevel 'All' 
-# Install-CustomModule -InputDir 'D:\Code\Repos\LD\PowerShell LD\WolfPack2' -UserLevel 'All'
+# Install-CustomModule -InputDir 'D:\Code\Repos\LD\PowerShell LD\WolfPack2' -UserLevel 'All' -unblock
